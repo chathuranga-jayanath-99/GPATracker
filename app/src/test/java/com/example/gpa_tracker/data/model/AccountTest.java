ceeaa378a11ae1d4803a93d42cb051bffd82c7ce
chathuranga-jayanath-99 <85400922+chathuranga-jayanath-99@users.noreply.github.com>
2022-09-22 23:59:32 +0530

    test - semester GPA cal updated with more subjects

--- app/src/test/java/com/example/gpa_tracker/data/model/AccountTest.java
+++ app/src/test/java/com/example/gpa_tracker/data/model/AccountTest.java
@@ -15,7 +15,7 @@ public class AccountTest {
     @Test
     public void getSemesterGpa() {
         Account account = new Account("1111", "1111", 4.2f, 8);
-        Semester semester = new Semester("1111", 8);
+        Semester semester = new Semester("1111", 7);
 
         // max gpa
         float maxGpa = 4.2f;
@@ -25,17 +25,29 @@ public class AccountTest {
         subjects.add(new Subject("CS-101", 2));
         subjects.add(new Subject("CS-102", 2));
         subjects.add(new Subject("CS-103", 3));
+        subjects.add(new Subject("CS-104", 2));
+        subjects.add(new Subject("CS-105", 3));
+        subjects.add(new Subject("CS-106", 3));
 
         // mark results
         subjects.get(0).setResult("A+");
-        subjects.get(1).setResult("A+");
-        subjects.get(2).setResult("A+");
-        subjects.get(3).setResult("A+");
+        subjects.get(1).setResult("B+");
+        subjects.get(2).setResult("A");
+        subjects.get(3).setResult("B+");
+        subjects.get(4).setResult("B+");
+        subjects.get(5).setResult("C+");
+        subjects.get(6).setResult("D");
 
-        for (int i = 0; i < 4; i++) {
+        // gained -> 3*4.2+2*3.3+2*4.0+3*3.3+2*3.3+3*2.3+3*1
+        // should -> (3*4+2*3)
+        float gained = 3*4.2f+2*3.3f+2*4.0f+3*3.3f+2*3.3f+3*2.3f+3*1f;
+        float maxGained =  (3*4+2*3)*4.2f;
+        float gpaCalculated = gained / maxGained * account.getMaxGpa();
+
+        for (int i = 0; i < subjects.size(); i++) {
             semester.addSubject(subjects.get(i));
         }
 
-        assertTrue(account.getSemesterGpa(semester)==4.2f);
+        assertTrue(account.getSemesterGpa(semester)==gpaCalculated);
     }
 }
