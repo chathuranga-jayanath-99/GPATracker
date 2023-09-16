bff22efe8346ad557aea037f6284089779053604
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-09-16 19:46:04 +0530

    add adapter layout for the semester subjects page

--- app/src/main/java/com/example/gpa_tracker/ui/SubjectListAdapter.java
+++ app/src/main/java/com/example/gpa_tracker/ui/SubjectListAdapter.java
@@ -4,40 +4,41 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.example.gpa_tracker.R;
 import com.example.gpa_tracker.data.model.Subject;
 
 import java.util.List;
 
-public class SubjectListAdapter extends BaseAdapter {
+public class SubjectListAdapter extends ArrayAdapter<Subject> {
     private Context context;
-    private List<Subject> subjectList;
-    private LayoutInflater inflater;
+    private int resource;
 
-    public SubjectListAdapter(Context context, List<Subject> subjectList) {
+    public SubjectListAdapter(@NonNull Context context, int resource, List<Subject> subjects) {
+        super(context, resource, subjects);
         this.context = context;
-        this.subjectList = subjectList;
-        this.inflater = LayoutInflater.from(context);
+        this.resource = resource;
     }
 
     @Override
-    public int getCount() {
-        return 0;
-    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String subjectName = getItem(position).getName();
+        float subjectCredits = getItem(position).getCredits();
+        String subjectResult = getItem(position).getResult();
 
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
+        convertView = LayoutInflater.from(context).inflate(resource, parent, false);
+        TextView tvModuleName = (TextView) convertView.findViewById(R.id.tvModuleName);
+        TextView tvModuleCredits = (TextView) convertView.findViewById(R.id.tvModuleCredits);
+        TextView tvModuleResult = (TextView) convertView.findViewById(R.id.tvModuleResult);
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        tvModuleName.setText(subjectName);
+        tvModuleCredits.setText(String.valueOf(subjectCredits));
+        tvModuleResult.setText(subjectResult);
+        return convertView;
     }
 }
