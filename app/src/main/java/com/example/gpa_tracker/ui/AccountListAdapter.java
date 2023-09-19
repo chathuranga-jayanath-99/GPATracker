508a8d68c2fe284cc16bf6ff88c6d1ea058fdb8f
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-09-19 07:56:40 +0530

    make account delete when holding

--- app/src/main/java/com/example/gpa_tracker/ui/AccountListAdapter.java
+++ app/src/main/java/com/example/gpa_tracker/ui/AccountListAdapter.java
@@ -1,13 +1,10 @@
 package com.example.gpa_tracker.ui;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,7 +14,6 @@ import com.example.gpa_tracker.R;
 import com.example.gpa_tracker.control.GpaTracker;
 import com.example.gpa_tracker.data.model.Account;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AccountListAdapter extends ArrayAdapter<Account> {
@@ -42,45 +38,8 @@ public class AccountListAdapter extends ArrayAdapter<Account> {
         convertView = LayoutInflater.from(context).inflate(resource, parent, false);
         TextView tvAccountId = (TextView) convertView.findViewById(R.id.tvAccountId);
         TextView tvAccountName = (TextView) convertView.findViewById(R.id.tvAccountName);
-        ImageButton imgAccountDeleteButton = (ImageButton) convertView.findViewById(R.id.imgAccountDeleteBtn);
-        imgAccountDeleteButton.setTag(account);
-
-        imgAccountDeleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showAccountDeleteConfirmation(view);
-                refresh();
-            }
-        });
         tvAccountId.setText(accountId);
         tvAccountName.setText(accountName);
         return convertView;
-    }
-
-    private void showAccountDeleteConfirmation(View view) {
-        Account selectedAccount = (Account) view.getTag();
-        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-        builder.setTitle("Confirmation");
-        builder.setMessage("Are you sure you want to delete account: " + selectedAccount.getName() + "-" + selectedAccount.getId() + "?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                gpaTracker.deleteAccount(selectedAccount.getId());
-                refresh();
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                
-            }
-        });
-        builder.create().show();
-    }
-
-    private void refresh() {
-        List<Account> accountList = gpaTracker.getAccounts();
-        AccountListAdapter accountListAdapter = new AccountListAdapter(context, R.layout.accounts_adapter_view_layout, accountList, gpaTracker, lvAccountsIdList);
-        lvAccountsIdList.setAdapter(accountListAdapter);
     }
 }
